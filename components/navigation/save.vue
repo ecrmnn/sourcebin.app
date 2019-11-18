9ae99f142901fa8cf845d17a8bb6980ba624bf6a
ecrmnn <danieleckermann@gmail.com>
2019-11-18 11:25:16 +0100

    Renames n to a service

--- components/navigation/save.vue
+++ components/navigation/save.vue
@@ -8,12 +8,12 @@
 
 <script>
 import axios from 'axios';
-import netlifyFunction from '../../n';
+import fn from '../../services/Netlify/Function';
 
 export default {
   methods: {
     save() {
-      axios.post(netlifyFunction('save'), {
+      axios.post(fn('save'), {
         mode: window.editor.getOption('mode'),
         theme: window.editor.getOption('theme'),
         body: window.editor.getValue(),
